a61df81c44ad2fd6846bab5af5d921e989fce86c
Suraj Kumar <surajkumarsonari@gmail.com>
2023-02-09 00:10:33 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,75 @@
+//Strategy Design Pattern: Design a vehicle system which has different types of drive capabilities.
+
+#include <iostream>
+using namespace std;
+
+class DriveStrategy
+{
+    public:
+    virtual void drive() = 0;
+};
+
+class NormalDriveStrategy : public DriveStrategy
+{
+    public:
+    void drive()
+    {
+        cout<<"Normal drive strategy"<<endl;
+    }
+};
+
+class SpecialDriveStrategy : public DriveStrategy
+{
+    public:
+    void drive()
+    {
+        cout<<"Special drive strategy"<<endl;
+    }
+};
+
+class Vehicle
+{
+    DriveStrategy *driv;
+    public:
+    Vehicle (DriveStrategy *driv)
+    {
+        this->driv = driv;
+    }
+    
+    void driveVehicle ()
+    {
+        driv->drive();
+    }
+};
+
+class SportsVehicle : public Vehicle
+{
+    public:
+    SportsVehicle():Vehicle(new SpecialDriveStrategy)
+    { }
+};
+
+class OffRoadVehicle : public Vehicle
+{
+    public:
+    OffRoadVehicle():Vehicle(new SpecialDriveStrategy)
+    { }
+};
+
+class PassengerVehicle : public Vehicle
+{
+    public:
+    PassengerVehicle():Vehicle(new NormalDriveStrategy)
+    { }
+};
+
+int main() {
+    OffRoadVehicle *ob1 = new OffRoadVehicle;
+    PassengerVehicle *ob2 = new PassengerVehicle;
+    SportsVehicle *ob3 = new SportsVehicle;
+    
+    ob1->driveVehicle();
+    ob2->driveVehicle();
+    ob3->driveVehicle();
+	return 0;
+}
